d1178f453d05165ded0ac1eca912df7f7a98c5ee
animeshc <animesh.chowdhury@gmail.com>
2012-07-06 15:58:47 -0400

    Added setLiteralExpression

--- core/src/main/java/xdi2/core/features/linkcontracts/NotExpression.java
+++ core/src/main/java/xdi2/core/features/linkcontracts/NotExpression.java
@@ -3,61 +3,74 @@ package xdi2.core.features.linkcontracts;
 import java.util.Iterator;
 
 import xdi2.core.ContextNode;
+import xdi2.core.Literal;
 import xdi2.core.constants.XDILinkContractConstants;
-
 
 public class NotExpression extends PolicyExpressionComponent {
 	private static final long serialVersionUID = 5732150467865911411L;
-	
-	public NotExpression(ContextNode c){
-		super(c);		
+
+	public NotExpression(ContextNode c) {
+		super(c);
 	}
-	
-	public static boolean isValid(ContextNode node){
-		return XDILinkContractConstants.XRI_SS_NOT.equals(node
-				.getArcXri());
+
+	public static boolean isValid(ContextNode node) {
+		return XDILinkContractConstants.XRI_SS_NOT.equals(node.getArcXri());
 	}
+
 	public static NotExpression fromContextNode(ContextNode c) {
 		if (!isValid(c)) {
 			return null;
 		}
 		return new NotExpression(c);
 	}
-	public String getLogicExpression(){
+
+	public String getLogicExpression() {
 		StringBuffer expr = new StringBuffer("(");
 		expr.append(" not ");
 		Iterator<ContextNode> allChildrenNodes = contextNode.getContextNodes();
-		
-		for(;allChildrenNodes.hasNext();){
-			
+
+		for (; allChildrenNodes.hasNext();) {
+
 			ContextNode childNode = allChildrenNodes.next();
-			
-			if(AndExpression.isValid(childNode)){
-				AndExpression andChild = AndExpression.fromContextNode(childNode);
-				expr.append(andChild.getLogicExpression());		
-				
-			}
-			else if(OrExpression.isValid(childNode)){
+
+			if (AndExpression.isValid(childNode)) {
+				AndExpression andChild = AndExpression
+						.fromContextNode(childNode);
+				expr.append(andChild.getLogicExpression());
+
+			} else if (OrExpression.isValid(childNode)) {
 				OrExpression orChild = OrExpression.fromContextNode(childNode);
 				expr.append(orChild.getLogicExpression());
-			}
-			else if(NotExpression.isValid(childNode)){
-				NotExpression notChild = NotExpression.fromContextNode(childNode);
+			} else if (NotExpression.isValid(childNode)) {
+				NotExpression notChild = NotExpression
+						.fromContextNode(childNode);
 				expr.append(notChild.getLogicExpression());
 			}
-		
-			
+
 		}
-		
-		if(contextNode.getLiteral() != null){
+
+		if (contextNode.getLiteral() != null) {
 			expr.append(contextNode.getLiteral().getLiteralData());
 		}
-			
+
 		expr.append(")");
 		return expr.toString();
 	}
-	public void addLiteralExpression(String expr){
+
+	public void addLiteralExpression(String expr) {
+		if(contextNode.getLiteral() != null){
+			contextNode.deleteLiteral();
+		}
 		contextNode.createLiteral(expr);
 	}
-	
+
+	public void setLiteralExpression(String expr) {
+		Literal literal = null;
+		if ((literal = contextNode.getLiteral()) == null) {
+			literal = contextNode.createLiteral(expr);
+		} else {
+			literal.setLiteralData(expr);
+		}
+	}
+
 }
