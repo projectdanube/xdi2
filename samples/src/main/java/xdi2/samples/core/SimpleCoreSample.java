fe769a20eb6fa7245614e3b3d581f67f46ada956
peacekeeper <markus.sabadello@gmail.com>
2012-09-14 12:12:23 +0200

    updated example

--- samples/src/main/java/xdi2/samples/core/SimpleCoreSample.java
+++ samples/src/main/java/xdi2/samples/core/SimpleCoreSample.java
@@ -4,26 +4,39 @@ import xdi2.core.ContextNode;
 import xdi2.core.Graph;
 import xdi2.core.impl.memory.MemoryGraphFactory;
 import xdi2.core.io.XDIWriterRegistry;
+import xdi2.core.xri3.impl.XRI3Segment;
 import xdi2.core.xri3.impl.XRI3SubSegment;
 
 public class SimpleCoreSample {
 
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        Graph graph = MemoryGraphFactory.getInstance().openGraph();
+		// create a simple graph
 
-        ContextNode root = graph.getRootContextNode();
-        ContextNode markus = root.createContextNode(new XRI3SubSegment("=markus"));
-        ContextNode name = markus.createContextNode(new XRI3SubSegment("+name"));
-        ContextNode email = markus.createContextNode(new XRI3SubSegment("+email"));
-        name.createLiteral("Markus Sabadello");
-        email.createLiteral("markus.sabadello@gmail.com");
+		Graph graph = MemoryGraphFactory.getInstance().openGraph();
 
-        System.out.println("Serialization in XDI/JSON: \n");
-        XDIWriterRegistry.forFormat("XDI/JSON", null).write(graph, System.out);
-        System.out.println();
+		ContextNode root = graph.getRootContextNode();
+		ContextNode markus = root.createContextNode(new XRI3SubSegment("=markus"));
+		ContextNode animesh = root.createContextNode(new XRI3SubSegment("=animesh"));
+		ContextNode name = markus.createContextNode(new XRI3SubSegment("+name"));
+		name.createLiteral("Markus Sabadello");
+		markus.createRelation(new XRI3Segment("+friend"), animesh);
 
-        System.out.println("Serialization in XDI statements:\n");
-        XDIWriterRegistry.forFormat("XDI DISPLAY", null).write(graph, System.out);
-    }
+		// write it in different serialization formats
+
+		System.out.println("Serialization in XDI/JSON: \n");
+		XDIWriterRegistry.forFormat("XDI/JSON", null).write(graph, System.out);
+		System.out.println();
+
+		System.out.println("Serialization in XDI statements:\n");
+		XDIWriterRegistry.forFormat("XDI DISPLAY", null).write(graph, System.out);
+
+		// write the statement associated with a context node
+
+		System.out.println("Statement associated with =markus: " + markus.getStatement());
+
+		// close the graph
+
+		graph.close();
+	}
 }
