534623991d3e425b94608e31d8ec0a07c74224cd
peacekeeper <markus.sabadello@gmail.com>
2012-09-14 12:15:05 +0200

    updated example

--- samples/src/main/java/xdi2/samples/core/SimpleCoreSample.java
+++ samples/src/main/java/xdi2/samples/core/SimpleCoreSample.java
@@ -2,6 +2,8 @@ package xdi2.samples.core;
 
 import xdi2.core.ContextNode;
 import xdi2.core.Graph;
+import xdi2.core.Literal;
+import xdi2.core.Relation;
 import xdi2.core.impl.memory.MemoryGraphFactory;
 import xdi2.core.io.XDIWriterRegistry;
 import xdi2.core.xri3.impl.XRI3Segment;
@@ -19,8 +21,8 @@ public class SimpleCoreSample {
 		ContextNode markus = root.createContextNode(new XRI3SubSegment("=markus"));
 		ContextNode animesh = root.createContextNode(new XRI3SubSegment("=animesh"));
 		ContextNode name = markus.createContextNode(new XRI3SubSegment("+name"));
-		name.createLiteral("Markus Sabadello");
-		markus.createRelation(new XRI3Segment("+friend"), animesh);
+		Relation relation = markus.createRelation(new XRI3Segment("+friend"), animesh);
+		Literal literal = name.createLiteral("Markus Sabadello");
 
 		// write it in different serialization formats
 
@@ -33,7 +35,9 @@ public class SimpleCoreSample {
 
 		// write the statement associated with a context node
 
-		System.out.println("Statement associated with =markus: " + markus.getStatement());
+		System.out.println("Statement associated with a context node: " + markus.getStatement());
+		System.out.println("Statement associated with a relation: " + relation.getStatement());
+		System.out.println("Statement associated with a literal: " + literal.getStatement());
 
 		// close the graph
 
