76514b790075350588fd429bb36f2f992b07869b
peacekeeper <markus.sabadello@gmail.com>
2012-09-29 09:46:13 +0200

    adding support for oauth token

--- messaging/src/main/java/xdi2/messaging/constants/XDIMessagingConstants.java
+++ messaging/src/main/java/xdi2/messaging/constants/XDIMessagingConstants.java
@@ -23,6 +23,7 @@ public final class XDIMessagingConstants {
 	public static final XRI3Segment XRI_S_TO_GRAPH = new XRI3Segment("$is()");
 
 	public static final XRI3Segment XRI_S_SECRET_TOKEN = new XRI3Segment("" + Multiplicity.entitySingletonArcXri(new XRI3SubSegment("$secret")) + Multiplicity.attributeSingletonArcXri(new XRI3SubSegment("$token")));
+	public static final XRI3Segment XRI_S_OAUTH_TOKEN = new XRI3Segment("" + Multiplicity.entitySingletonArcXri(new XRI3SubSegment("$oauth")) + Multiplicity.attributeSingletonArcXri(new XRI3SubSegment("$token")));
 
 	public static final XRI3SubSegment XRI_SS_MSG = new XRI3SubSegment("$msg");
 	public static final XRI3SubSegment XRI_SS_GET = new XRI3SubSegment("$get");
