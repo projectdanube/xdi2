b524683aa5c6fd54fa7f2bb4b2958247ec9a87a5
peacekeeper <markus.sabadello@gmail.com>
2012-10-01 12:59:04 +0200

    new GraphFactory ability to load graph from stream, reader, file, url, etc

--- core/src/main/java/xdi2/core/GraphFactory.java
+++ core/src/main/java/xdi2/core/GraphFactory.java
@@ -1,6 +1,10 @@
 package xdi2.core;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URL;
 import java.util.Properties;
 
 import xdi2.core.exceptions.Xdi2ParseException;
@@ -14,33 +18,57 @@ import xdi2.core.io.MimeType;
 public interface GraphFactory {
 
 	/**
-	 * Creates/opens a graph.
+	 * Opens a graph.
 	 * @return An already existing or new graph.
 	 */
 	public Graph openGraph() throws IOException;
 
 	/**
-	 * Creates/opens a graph.
+	 * Opens a graph.
 	 * @param identifier An optional identifier to distinguish graphs from one another.
 	 * @return An already existing or new graph.
 	 */
 	public Graph openGraph(String identifier) throws IOException;
 
 	/**
-	 * Creates/opens a graph and fills it with content in one of the serialization formats.
-	 * @return An already existing or new graph.
+	 * Opens a graph and fills it with content in one of the serialization formats.
+	 * @return A new graph.
+	 */
+	public Graph loadGraph(URL url) throws IOException, Xdi2ParseException;
+
+	/**
+	 * Opens a graph and fills it with content in one of the serialization formats.
+	 * @return A new graph.
+	 */
+	public Graph loadGraph(File file) throws IOException, Xdi2ParseException;
+
+	/**
+	 * Opens a graph and fills it with content in one of the serialization formats.
+	 * @return A new graph.
+	 */
+	public Graph loadGraph(InputStream inputStream) throws IOException, Xdi2ParseException;
+
+	/**
+	 * Opens a graph and fills it with content in one of the serialization formats.
+	 * @return A new graph.
+	 */
+	public Graph loadGraph(Reader reader) throws IOException, Xdi2ParseException;
+
+	/**
+	 * Opens a graph and fills it with content in one of the serialization formats.
+	 * @return A new graph.
 	 */
 	public Graph parseGraph(String graph) throws IOException, Xdi2ParseException;
 
 	/**
-	 * Creates/opens a graph and fills it with content in one of the serialization formats.
-	 * @return An already existing or new graph.
+	 * Opens a graph and fills it with content in one of the serialization formats.
+	 * @return A new graph.
 	 */
 	public Graph parseGraph(String graph, String format, Properties parameters) throws IOException, Xdi2ParseException;
 
 	/**
-	 * Creates/opens a graph and fills it with content in one of the serialization formats.
-	 * @return An already existing or new graph.
+	 * Opens a graph and fills it with content in one of the serialization formats.
+	 * @return A new graph.
 	 */
 	public Graph parseGraph(String graph, MimeType mimeType) throws IOException, Xdi2ParseException;
 }
