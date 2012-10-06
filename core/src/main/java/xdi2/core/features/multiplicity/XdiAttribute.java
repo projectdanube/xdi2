14e5bd522f022975f520ca1d3bbf6400f3ff1f51
peacekeeper <markus.sabadello@gmail.com>
2012-10-06 23:42:05 +0200

    adding abstract superclasses XdiAttribute and XdiEntity

---
+++ core/src/main/java/xdi2/core/features/multiplicity/XdiAttribute.java
@@ -0,0 +1,13 @@
+package xdi2.core.features.multiplicity;
+
+import xdi2.core.ContextNode;
+
+public abstract class XdiAttribute extends XdiSubGraph {
+
+	private static final long serialVersionUID = -4944747722611979113L;
+
+	protected XdiAttribute(ContextNode contextNode) {
+
+		super(contextNode);
+	}
+}
---
+++ core/src/main/java/xdi2/core/features/multiplicity/XdiEntity.java
@@ -0,0 +1,13 @@
+package xdi2.core.features.multiplicity;
+
+import xdi2.core.ContextNode;
+
+public abstract class XdiEntity extends XdiSubGraph {
+
+	private static final long serialVersionUID = -1976646316893343570L;
+
+	protected XdiEntity(ContextNode contextNode) {
+
+		super(contextNode);
+	}
+}
