fc9b94028ada9d80f8f0ee0d480801ed66a40ac1
peacekeeper <markus.sabadello@gmail.com>
2012-11-08 10:30:39 +0100

    more updates to server debug page

--- server/src/main/java/xdi2/server/interceptor/impl/DebugEndpointServletInterceptor.java
+++ server/src/main/java/xdi2/server/interceptor/impl/DebugEndpointServletInterceptor.java
@@ -1,5 +1,6 @@
 package xdi2.server.interceptor.impl;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -15,9 +16,11 @@ import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
 
+import xdi2.core.plugins.PluginsLoader;
 import xdi2.messaging.target.MessagingTarget;
 import xdi2.server.EndpointServlet;
 import xdi2.server.RequestInfo;
+import xdi2.server.exceptions.Xdi2ServerException;
 import xdi2.server.factory.MessagingTargetFactory;
 import xdi2.server.interceptor.AbstractEndpointServletInterceptor;
 
@@ -39,6 +42,42 @@ public class DebugEndpointServletInterceptor extends AbstractEndpointServletInte
 		this.velocityEngine.init();
 	}
 
+
+	@Override
+	public boolean processPostRequest(EndpointServlet endpointServlet, HttpServletRequest request, HttpServletResponse response, RequestInfo requestInfo, MessagingTarget messagingTarget) throws ServletException, IOException {
+
+		if (! requestInfo.getRequestPath().equals("/")) return false;
+
+		String cmd = request.getParameter("cmd");
+		String cmdMessagingTargetPath = request.getParameter("messagingtargetpath");
+		String cmdMessagingTargetFactoryPath = request.getParameter("messagingtargetfactorypath");
+
+		if ("reload".equals(cmd)) {
+
+			try {
+
+				endpointServlet.getEndpointRegistry().reload();
+			} catch (Xdi2ServerException ex) {
+
+				throw new ServletException(ex.getMessage(), ex);
+			}
+		}
+
+		if ("unmount_messaging_target".equals(cmd) && cmdMessagingTargetPath != null) {
+
+			MessagingTarget cmdMessagingTarget = endpointServlet.getEndpointRegistry().getMessagingTarget(cmdMessagingTargetPath);
+			if (cmdMessagingTarget != null) endpointServlet.getEndpointRegistry().unmountMessagingTarget(cmdMessagingTarget);
+		}
+
+		if ("unmount_messaging_target_factory".equals(cmd) && cmdMessagingTargetFactoryPath != null) {
+
+			MessagingTargetFactory cmdMessagingTargetFactory = endpointServlet.getEndpointRegistry().getMessagingTargetFactory(cmdMessagingTargetFactoryPath);
+			if (cmdMessagingTargetFactory != null) endpointServlet.getEndpointRegistry().unmountMessagingTargetFactory(cmdMessagingTargetFactory);
+		}
+
+		return this.processGetRequest(endpointServlet, request, response, requestInfo, messagingTarget);
+	}
+
 	@Override
 	public boolean processGetRequest(EndpointServlet endpointServlet, HttpServletRequest request, HttpServletResponse response, RequestInfo requestInfo, MessagingTarget messagingTarget) throws ServletException, IOException {
 
@@ -46,6 +85,7 @@ public class DebugEndpointServletInterceptor extends AbstractEndpointServletInte
 
 		// prepare velocity
 
+		File[] pluginFiles = PluginsLoader.getFiles();
 		List<MessagingTarget> messagingTargets = endpointServlet.getEndpointRegistry().getMessagingTargets();
 		Map<String, MessagingTarget> messagingTargetsByPath = endpointServlet.getEndpointRegistry().getMessagingTargetsByPath();
 		List<MessagingTargetFactory> messagingTargetFactorys = endpointServlet.getEndpointRegistry().getMessagingTargetFactorys();
@@ -54,6 +94,7 @@ public class DebugEndpointServletInterceptor extends AbstractEndpointServletInte
 		VelocityContext context = new VelocityContext();
 		context.put("endpointservlet", endpointServlet);
 		context.put("requestinfo", requestInfo);
+		context.put("pluginfiles", pluginFiles);
 		context.put("messagingtargets", messagingTargets);
 		context.put("messagingtargetsbypath", messagingTargetsByPath);
 		context.put("messagingtargetfactorys", messagingTargetFactorys);
