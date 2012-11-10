0d514335c099486867f3a50f1372305a30870c7e
peacekeeper <markus.sabadello@gmail.com>
2012-11-10 16:26:27 +0100

    more functionality for the server debug page

--- server/src/main/java/xdi2/server/interceptor/impl/DebugEndpointServletInterceptor.java
+++ server/src/main/java/xdi2/server/interceptor/impl/DebugEndpointServletInterceptor.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Reader;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +19,10 @@ import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
 
+import xdi2.core.Graph;
+import xdi2.core.exceptions.Xdi2ParseException;
+import xdi2.core.io.XDIReader;
+import xdi2.core.io.XDIReaderRegistry;
 import xdi2.core.io.XDIWriter;
 import xdi2.core.io.XDIWriterRegistry;
 import xdi2.core.plugins.PluginsLoader;
@@ -57,9 +62,10 @@ public class DebugEndpointServletInterceptor extends AbstractEndpointServletInte
 		String cmdMessagingTargetPath = request.getParameter("messagingtargetpath");
 		String cmdMessagingTargetFactoryPath = request.getParameter("messagingtargetfactorypath");
 		String format = request.getParameter("format");
-		String writeContexts = request.getParameter("writeContexts");
-		String writeOrdered = request.getParameter("writeOrdered");
-		String writePretty = request.getParameter("writePretty");
+		String writecontexts = request.getParameter("writecontexts");
+		String writeordered = request.getParameter("writeordered");
+		String writepretty = request.getParameter("writepretty");
+		String graphstring = request.getParameter("graphstring");
 
 		if ("reload".equals(cmd)) {
 
@@ -88,15 +94,21 @@ public class DebugEndpointServletInterceptor extends AbstractEndpointServletInte
 
 			MessagingTarget cmdMessagingTarget = endpointServlet.getEndpointRegistry().getMessagingTarget(cmdMessagingTargetPath);
 
-			// serialize graph
+			// prepare format and parameters
+
+			if (format == null) {
+
+				format = XDIWriterRegistry.getDefault().getFormat();
+				writecontexts = null;
+				writeordered = "on";
+				writepretty = "on";
+			}
 
 			Properties xdiWriterParameters = new Properties();
 
-			if ("on".equals(writeContexts)) xdiWriterParameters.setProperty(XDIWriterRegistry.PARAMETER_CONTEXTS, "1");
-			if ("on".equals(writeOrdered)) xdiWriterParameters.setProperty(XDIWriterRegistry.PARAMETER_ORDERED, "1");
-			if ("on".equals(writePretty)) xdiWriterParameters.setProperty(XDIWriterRegistry.PARAMETER_PRETTY, "1");
-
-			if (format == null) XDIWriterRegistry.getDefault().getFormat();
+			if ("on".equals(writecontexts)) xdiWriterParameters.setProperty(XDIWriterRegistry.PARAMETER_CONTEXTS, "1");
+			if ("on".equals(writeordered)) xdiWriterParameters.setProperty(XDIWriterRegistry.PARAMETER_ORDERED, "1");
+			if ("on".equals(writepretty)) xdiWriterParameters.setProperty(XDIWriterRegistry.PARAMETER_PRETTY, "1");
 
 			XDIWriter xdiWriter = XDIWriterRegistry.forFormat(format, xdiWriterParameters);
 
@@ -104,16 +116,70 @@ public class DebugEndpointServletInterceptor extends AbstractEndpointServletInte
 
 			xdiWriter.write(((GraphMessagingTarget) cmdMessagingTarget).getGraph(), stringWriter);
 
-			String graphString = stringWriter.getBuffer().toString();
+			graphstring = stringWriter.getBuffer().toString();
 
 			// prepare velocity
 
 			VelocityContext context = new VelocityContext();
 			context.put("endpointservlet", endpointServlet);
 			context.put("requestinfo", requestInfo);
-			context.put("messagingtarget", messagingTarget);
+			context.put("messagingtarget", cmdMessagingTarget);
 			context.put("messagingtargetpath", cmdMessagingTargetPath);
-			context.put("graph", graphString);
+			context.put("format", format);
+			context.put("writecontexts", writecontexts);
+			context.put("writeordered", writeordered);
+			context.put("writepretty", writepretty);
+			context.put("graphstring", graphstring);
+
+			// send response
+
+			Reader reader = new InputStreamReader(this.getClass().getResourceAsStream("debug-edit.vm"));
+			PrintWriter writer = response.getWriter();
+
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.setContentType("text/html");
+			this.velocityEngine.evaluate(context, writer, "debug-edit.vm", reader);
+			writer.close();
+
+			// done
+
+			return true;
+		}
+
+		if ("save_messaging_target".equals(cmd) && cmdMessagingTargetPath != null) {
+
+			MessagingTarget cmdMessagingTarget = endpointServlet.getEndpointRegistry().getMessagingTarget(cmdMessagingTargetPath);
+
+			// parse graph
+
+			XDIReader xdiReader = XDIReaderRegistry.getAuto();
+
+			Graph graph = ((GraphMessagingTarget) cmdMessagingTarget).getGraph();
+			
+			String error = null;
+			
+			try {
+
+				graph.clear();
+				xdiReader.read(graph, new StringReader(graphstring));
+			} catch (Xdi2ParseException ex) {
+
+				error = ex.getMessage();
+			}
+
+			// prepare velocity
+
+			VelocityContext context = new VelocityContext();
+			context.put("endpointservlet", endpointServlet);
+			context.put("requestinfo", requestInfo);
+			context.put("messagingtarget", cmdMessagingTarget);
+			context.put("messagingtargetpath", cmdMessagingTargetPath);
+			context.put("format", format);
+			context.put("writecontexts", writecontexts);
+			context.put("writeordered", writeordered);
+			context.put("writepretty", writepretty);
+			context.put("graphstring", graphstring);
+			context.put("error", error);
 
 			// send response
 
