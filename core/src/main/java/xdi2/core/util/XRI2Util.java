15361bda2f5bc5a4110a3f8918087eb49c2c251e
peacekeeper <markus.sabadello@gmail.com>
2013-04-29 10:52:18 +0200

    xri2 cloudnumber support

--- core/src/main/java/xdi2/core/util/XRI2Util.java
+++ core/src/main/java/xdi2/core/util/XRI2Util.java
@@ -6,6 +6,7 @@ import java.net.URLEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import xdi2.core.constants.XDIConstants;
 import xdi2.core.xri3.XDI3Segment;
 import xdi2.core.xri3.XDI3SubSegment;
 
@@ -21,7 +22,7 @@ public final class XRI2Util {
 	private XRI2Util() { }
 
 	/**
-	 * Maps an XRI 2.0 Canonical ID to a cloud number.
+	 * Maps an XRI 2.0 Canonical ID to a Cloud Number.
 	 */
 	public static XDI3Segment canonicalIdToCloudNumber(String canonicalId) {
 
@@ -42,7 +43,7 @@ public final class XRI2Util {
 
 		StringBuilder builder = new StringBuilder();
 
-		builder.append("[" + cs + "]!:uuid:");
+		builder.append("[" + cs + "]" + XDIConstants.CS_BANG + ":uuid:");
 		builder.append(parts[0]);
 		builder.append(parts[1]);
 		builder.append("-");
@@ -57,6 +58,60 @@ public final class XRI2Util {
 		builder.append(parts[3]);
 
 		return XDI3Segment.create(builder.toString());
+	}
+
+	/**
+	 * Maps a Cloud Number to an XRI 2.0 Canonical ID.
+	 */
+	public static String cloudNumberToCanonicalId(XDI3Segment cloudNumber) {
+
+		if (log.isTraceEnabled()) log.trace("cloudNumberToCanonicalId(" + cloudNumber + ")");
+
+		if (cloudNumber.getNumSubSegments() != 2) return null;
+
+		if (! cloudNumber.getFirstSubSegment().isClassXs()) return null;
+		if (cloudNumber.getFirstSubSegment().hasLiteral()) return null;
+		if (cloudNumber.getFirstSubSegment().hasXRef()) return null;
+
+		char cs = cloudNumber.getFirstSubSegment().getCs().charValue();
+
+		if (! XDIConstants.CS_BANG.equals(cloudNumber.getLastSubSegment().getCs())) return null;
+		if (! cloudNumber.getLastSubSegment().hasLiteral()) return null;
+		if (cloudNumber.getLastSubSegment().hasXRef()) return null;
+		if (! cloudNumber.getLastSubSegment().getLiteral().startsWith(":uuid")) return null;
+		if (cloudNumber.getLastSubSegment().getLiteral().length() != 42) return null;
+
+		String[] parts = new String[4];
+		parts[0] = cloudNumber.getLastSubSegment().getLiteral().substring(6, 10);
+		parts[1] = cloudNumber.getLastSubSegment().getLiteral().substring(10, 14);
+		parts[2] = cloudNumber.getLastSubSegment().getLiteral().substring(15, 19);
+		parts[3] = cloudNumber.getLastSubSegment().getLiteral().substring(20, 24);
+
+		if (! parts[0].equals(cloudNumber.getLastSubSegment().getLiteral().substring(25, 29))) return null;
+		if (! parts[1].equals(cloudNumber.getLastSubSegment().getLiteral().substring(30, 34))) return null;
+		if (! parts[2].equals(cloudNumber.getLastSubSegment().getLiteral().substring(34, 38))) return null;
+		if (! parts[3].equals(cloudNumber.getLastSubSegment().getLiteral().substring(38, 42))) return null;
+
+		for (int i=0; i<parts.length; i++) {
+
+			if (parts[i].length() > 4) return null;
+			if (parts[i].indexOf('-') != -1) return null;
+			parts[i] = parts[i].toUpperCase();
+			while (parts[i].charAt(0) == '0') parts[i] = parts[i].substring(1);
+		}
+
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("" + cs + XDIConstants.CS_BANG);
+		builder.append(parts[0]);
+		builder.append(".");
+		builder.append(parts[1]);
+		builder.append(".");
+		builder.append(parts[2]);
+		builder.append(".");
+		builder.append(parts[3]);
+
+		return builder.toString();
 	}
 
 	/**
--- core/src/test/java/xdi2/tests/core/util/XRI2UtilTest.java
+++ core/src/test/java/xdi2/tests/core/util/XRI2UtilTest.java
@@ -2,15 +2,22 @@ package xdi2.tests.core.util;
 
 import junit.framework.TestCase;
 import xdi2.core.util.XRI2Util;
+import xdi2.core.xri3.XDI3Segment;
 
 public class XRI2UtilTest extends TestCase {
 
 	public void testCanonicalIdToCloudnumber() throws Exception {
 
 		assertEquals(XRI2Util.canonicalIdToCloudNumber("=!91F2.8153.F600.AE24"), "[=]!:uuid:91f28153-f600-ae24-91f2-8153f600ae24");
-		assertEquals(XRI2Util.canonicalIdToCloudNumber("@!F83.62B1.44F.2813"), "[@]!:uuid:0f8362b1-044f-2813-0f83-62b1044f2813");
 		assertEquals(XRI2Util.canonicalIdToCloudNumber("@!F83.62B1.044F.2813"), "[@]!:uuid:0f8362b1-044f-2813-0f83-62b1044f2813");
 		assertNull(XRI2Util.canonicalIdToCloudNumber("=!91F2.8153.F600"));
+	}
+
+	public void testCloudNumberToCanonicalId() throws Exception {
+
+		assertEquals(XRI2Util.cloudNumberToCanonicalId(XDI3Segment.create("[=]!:uuid:91f28153-f600-ae24-91f2-8153f600ae24")), "=!91F2.8153.F600.AE24");
+		assertEquals(XRI2Util.cloudNumberToCanonicalId(XDI3Segment.create("[@]!:uuid:0f8362b1-044f-2813-0f83-62b1044f2813")), "@!F83.62B1.44F.2813");
+		assertNull(XRI2Util.cloudNumberToCanonicalId(XDI3Segment.create("[@]!:uuid:0f8362b1-044f")));
 	}
 
 	public void testTypeToXdiEntitySingletonArcXri() throws Exception {
