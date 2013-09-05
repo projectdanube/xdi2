0e707b72241ff68ece6256833af14b9c37b8b9a3
peacekeeper <markus.sabadello@gmail.com>
2013-09-05 11:07:42 +0200

    update to raw json reader, but is still buggy

--- core/src/main/java/xdi2/core/io/readers/XDIRawJSONReader.java
+++ core/src/main/java/xdi2/core/io/readers/XDIRawJSONReader.java
@@ -3,32 +3,25 @@ package xdi2.core.io.readers;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.security.MessageDigest;
 import java.util.Map.Entry;
 import java.util.Properties;
 
-import org.apache.commons.codec.binary.Base64;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import xdi2.core.Graph;
-import xdi2.core.constants.XDIConstants;
 import xdi2.core.exceptions.Xdi2GraphException;
 import xdi2.core.exceptions.Xdi2ParseException;
-import xdi2.core.exceptions.Xdi2RuntimeException;
 import xdi2.core.features.dictionary.Dictionary;
 import xdi2.core.features.nodetypes.XdiAbstractContext;
 import xdi2.core.features.nodetypes.XdiAttributeClass;
-import xdi2.core.features.nodetypes.XdiAttributeInstanceUnordered;
+import xdi2.core.features.nodetypes.XdiAttributeInstanceOrdered;
 import xdi2.core.features.nodetypes.XdiAttributeSingleton;
 import xdi2.core.features.nodetypes.XdiContext;
 import xdi2.core.features.nodetypes.XdiEntityClass;
-import xdi2.core.features.nodetypes.XdiEntityInstanceUnordered;
+import xdi2.core.features.nodetypes.XdiEntityInstanceOrdered;
 import xdi2.core.features.nodetypes.XdiEntitySingleton;
 import xdi2.core.features.nodetypes.XdiValue;
+import xdi2.core.impl.AbstractLiteral;
 import xdi2.core.io.AbstractXDIReader;
 import xdi2.core.io.MimeType;
-import xdi2.core.xri3.XDI3Segment;
 import xdi2.core.xri3.XDI3SubSegment;
 
 import com.google.gson.Gson;
@@ -42,16 +35,9 @@ public class XDIRawJSONReader extends AbstractXDIReader {
 
 	private static final long serialVersionUID = 5574875512968150162L;
 
-	private static final Logger log = LoggerFactory.getLogger(XDIRawJSONReader.class);
-
 	public static final String FORMAT_NAME = "RAW JSON";
 	public static final String FILE_EXTENSION = null;
 	public static final MimeType MIME_TYPE = null;
-
-	public static final XDI3Segment XRI_DATATYPE_JSON_NUMBER = XDI3Segment.create("+$json$number");
-	public static final XDI3Segment XRI_DATATYPE_JSON_TRUE = XDI3Segment.create("+$json$true");
-	public static final XDI3Segment XRI_DATATYPE_JSON_FALSE = XDI3Segment.create("+$json$false");
-	public static final XDI3Segment XRI_DATATYPE_JSON_NULL = XDI3Segment.create("+$json$null");
 
 	private static final Gson gson = new GsonBuilder().disableHtmlEscaping().serializeNulls().create();
 
@@ -83,48 +69,54 @@ public class XDIRawJSONReader extends AbstractXDIReader {
 				XDI3SubSegment arcXri = Dictionary.nativeIdentifierToInstanceXri(key);
 
 				readJsonArray(xdiContext, arcXri, (JsonArray) jsonElement);
-			} else if (jsonElement instanceof JsonPrimitive && ((JsonPrimitive) jsonElement).isString()){
+			} else if (jsonElement instanceof JsonPrimitive) {
 
 				XDI3SubSegment arcXri = Dictionary.nativeIdentifierToInstanceXri(key);
 
 				XdiAttributeSingleton xdiAttributeSingleton = xdiContext.getXdiAttributeSingleton(arcXri, true);
-
 				XdiValue xdiValue = xdiAttributeSingleton.getXdiValue(true);
-
-				xdiValue.getContextNode().setLiteral(((JsonPrimitive) jsonElement).getAsString());
+				xdiValue.getContextNode().setLiteral(AbstractLiteral.jsonElementToLiteralData(jsonElement));
 			}
 		}
 	}
 
 	private static void readJsonArray(XdiContext xdiContext, XDI3SubSegment arcXri, JsonArray jsonArray) {
 
-		for (JsonElement jsonElement : jsonArray) {
+		if (arcXri == null) {
 
-			XDI3SubSegment jsonContentId = jsonContentId(jsonElement);
+			arcXri = XDI3SubSegment.create("$array");
+		}
+
+		long index = 0;
+
+		for (JsonElement jsonElement : jsonArray) {
 
 			if (jsonElement instanceof JsonObject) {
 
 				XdiEntityClass xdiEntityClass = xdiContext.getXdiEntityClass(arcXri, true);
 
-				XdiEntityInstanceUnordered xdiEntityInstance = xdiEntityClass.setXdiInstanceUnordered(jsonContentId);
+				XdiEntityInstanceOrdered xdiEntityInstance = xdiEntityClass.setXdiInstanceOrdered(index);
 				readJsonObject(xdiEntityInstance, (JsonObject) jsonElement);
 			} else if (jsonElement instanceof JsonArray) {
 
-				throw new RuntimeException("Nested JSON arrays not supported in XDI mapping.");
+				XdiEntityClass xdiEntityClass = xdiContext.getXdiEntityClass(arcXri, true);
+
+				XdiEntityInstanceOrdered xdiEntityInstance = xdiEntityClass.setXdiInstanceOrdered(index);
+				readJsonArray(xdiEntityInstance, null, (JsonArray) jsonElement);
 			} else {
 
 				XdiAttributeClass xdiAttributeClass = xdiContext.getXdiAttributeClass(arcXri, true);
 
-				XdiAttributeInstanceUnordered xdiAttributeInstance = xdiAttributeClass.setXdiInstanceUnordered(jsonContentId);
-
+				XdiAttributeInstanceOrdered xdiAttributeInstance = xdiAttributeClass.setXdiInstanceOrdered(index);
 				XdiValue xdiValue = xdiAttributeInstance.getXdiValue(true);
-
-				xdiValue.getContextNode().setLiteral(jsonElement.toString());
+				xdiValue.getContextNode().setLiteral(AbstractLiteral.jsonElementToLiteralData(jsonElement));
 			}
+
+			index++;
 		}
 	}
 
-	private static XDI3SubSegment jsonContentId(JsonElement jsonElement) {
+	/*	private static XDI3SubSegment jsonContentId(JsonElement jsonElement) {
 
 		try {
 
@@ -144,20 +136,32 @@ public class XDIRawJSONReader extends AbstractXDIReader {
 
 			throw new Xdi2RuntimeException(ex.getMessage(), ex);
 		}
+	}*/
+
+	public void read(Graph graph, JsonObject jsonObject) throws IOException, Xdi2ParseException {
+
+		readJsonObject(XdiAbstractContext.fromContextNode(graph.getRootContextNode()), jsonObject);
 	}
 
-	public void read(Graph graph, JsonObject graphObject) throws IOException, Xdi2ParseException {
+	public void read(Graph graph, JsonArray jsonArray) throws IOException, Xdi2ParseException {
 
-		readJsonObject(XdiAbstractContext.fromContextNode(graph.getRootContextNode()), graphObject);
+		readJsonArray(XdiAbstractContext.fromContextNode(graph.getRootContextNode()), null, jsonArray);
 	}
 
 	private void read(Graph graph, BufferedReader bufferedReader) throws IOException, Xdi2ParseException {
 
 		JsonElement jsonRootElement = gson.getAdapter(JsonObject.class).fromJson(bufferedReader);
 
-		if (! (jsonRootElement instanceof JsonObject)) throw new Xdi2ParseException("JSON must be an object: " + jsonRootElement);
+		if (jsonRootElement instanceof JsonObject) {
 
-		this.read(graph, (JsonObject) jsonRootElement);
+			this.read(graph, (JsonObject) jsonRootElement);
+		} else if (jsonRootElement instanceof JsonArray) {
+
+			this.read(graph, (JsonArray) jsonRootElement);
+		} else {
+
+			throw new Xdi2ParseException("JSON must be an object or array: " + jsonRootElement);
+		}
 	}
 
 	@Override
