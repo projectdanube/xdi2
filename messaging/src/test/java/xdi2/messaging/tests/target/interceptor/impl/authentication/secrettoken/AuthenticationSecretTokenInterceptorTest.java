e199b5da7d585f28b45c084544039ea2f43ab081
peacekeeper <markus.sabadello@gmail.com>
2013-10-01 19:32:02 +0200

    updated test

--- messaging/src/test/java/xdi2/messaging/tests/target/interceptor/impl/authentication/secrettoken/AuthenticationSecretTokenInterceptorTest.java
+++ messaging/src/test/java/xdi2/messaging/tests/target/interceptor/impl/authentication/secrettoken/AuthenticationSecretTokenInterceptorTest.java
@@ -1,6 +1,11 @@
 package xdi2.messaging.tests.target.interceptor.impl.authentication.secrettoken;
 
+import java.util.Collections;
+
 import junit.framework.TestCase;
+import xdi2.core.xri3.XDI3Segment;
+import xdi2.messaging.Message;
+import xdi2.messaging.MessageEnvelope;
 import xdi2.messaging.target.interceptor.impl.authentication.secrettoken.DigestSecretTokenAuthenticator;
 import xdi2.messaging.target.interceptor.impl.authentication.secrettoken.StaticSecretTokenAuthenticator;
 
@@ -15,6 +20,8 @@ public class AuthenticationSecretTokenInterceptorTest extends TestCase {
 
 	public static String[] VALID_SALTS = new String[] { GLOBAL_SALT, LOCAL_SALT };
 	public static String[] INVALID_SALTS = new String[] { "3b97782d:b130:4906:b41d:f83b6968765f", "0-0-0-0-0" };
+
+	public static XDI3Segment SENDER_XRI = XDI3Segment.create("msender");
 
 	public void testDigestSecretTokenAuthenticatorWithChosenLocalSalt() throws Exception {
 
@@ -56,7 +63,10 @@ public class AuthenticationSecretTokenInterceptorTest extends TestCase {
 
 		StaticSecretTokenAuthenticator staticSecretTokenAuthenticator = new StaticSecretTokenAuthenticator();
 		staticSecretTokenAuthenticator.setGlobalSalt(GLOBAL_SALT);
+		staticSecretTokenAuthenticator.setLocalSaltAndDigestSecretTokens(Collections.singletonMap(SENDER_XRI, LOCAL_SALT_AND_DIGEST_SECRET_TOKEN));
 
-		assertTrue(staticSecretTokenAuthenticator.authenticate(LOCAL_SALT_AND_DIGEST_SECRET_TOKEN, SECRET_TOKEN));
+		Message message = new MessageEnvelope().getMessage(SENDER_XRI, true);
+
+		assertTrue(staticSecretTokenAuthenticator.authenticate(message, SECRET_TOKEN));
 	}
 }
