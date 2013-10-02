3f3f8f90da2824b58318b44264e68212d55b5a9a
peacekeeper <markus.sabadello@gmail.com>
2013-10-02 13:13:53 +0200

    moving authentication-related contributors and interceptors from messaging to server

--- messaging/src/test/java/xdi2/messaging/tests/AllTests.java
+++ messaging/src/test/java/xdi2/messaging/tests/AllTests.java
@@ -6,13 +6,12 @@ import xdi2.messaging.tests.basic.BasicTest;
 import xdi2.messaging.tests.http.AcceptHeaderTest;
 import xdi2.messaging.tests.target.contributor.ContributorTest;
 import xdi2.messaging.tests.target.impl.graph.BDBKeyValueGraphMessagingTargetTest;
-import xdi2.messaging.tests.target.impl.graph.FileWrapperGraphMessagingTargetTest;
 import xdi2.messaging.tests.target.impl.graph.FileJSONGraphMessagingTargetTest;
-import xdi2.messaging.tests.target.impl.graph.MemoryJSONGraphMessagingTargetTest;
+import xdi2.messaging.tests.target.impl.graph.FileWrapperGraphMessagingTargetTest;
 import xdi2.messaging.tests.target.impl.graph.MapGraphMessagingTargetTest;
 import xdi2.messaging.tests.target.impl.graph.MemoryGraphMessagingTargetTest;
+import xdi2.messaging.tests.target.impl.graph.MemoryJSONGraphMessagingTargetTest;
 import xdi2.messaging.tests.target.impl.graph.PropertiesKeyValueGraphMessagingTargetTest;
-import xdi2.messaging.tests.target.interceptor.impl.authentication.secrettoken.AuthenticationSecretTokenInterceptorTest;
 import xdi2.messaging.tests.target.interceptor.impl.linkcontract.LinkContractInterceptorTest;
 
 public class AllTests {
@@ -30,7 +29,6 @@ public class AllTests {
 		suite.addTestSuite(FileJSONGraphMessagingTargetTest.class);
 		suite.addTestSuite(FileWrapperGraphMessagingTargetTest.class);
 		suite.addTestSuite(LinkContractInterceptorTest.class);
-		suite.addTestSuite(AuthenticationSecretTokenInterceptorTest.class);
 		suite.addTestSuite(ContributorTest.class);
 		//$JUnit-END$
 		return suite;
---
+++ server/src/test/java/xdi2/server/tests/AllTests.java
@@ -0,0 +1,18 @@
+package xdi2.server.tests;
+
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import xdi2.messaging.tests.target.interceptor.impl.authentication.secrettoken.AuthenticationSecretTokenInterceptorTest;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		//$JUnit-BEGIN$
+		suite.addTestSuite(AuthenticationSecretTokenInterceptorTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
