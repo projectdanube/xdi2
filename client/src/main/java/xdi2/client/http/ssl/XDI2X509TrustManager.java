b69b619119a1ab782ec0cc813ceff89144cee463
peacekeeper <markus@projectdanube.org>
2014-04-21 22:38:03 +0200

    hopefully making the SSL certs work now..

--- client/src/main/java/xdi2/client/http/ssl/XDI2X509TrustManager.java
+++ client/src/main/java/xdi2/client/http/ssl/XDI2X509TrustManager.java
@@ -13,49 +13,68 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import xdi2.client.http.XDIHttpClient;
+
 public class XDI2X509TrustManager implements X509TrustManager {
+
+	private static final Logger log = LoggerFactory.getLogger(XDIHttpClient.class);
 
 	private static List<X509TrustManager> tms;
 
 	@Override
 	public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {  
 
-		CertificateException ex = null;
+		CertificateException cex = null;
+		RuntimeException rex = null;
 
 		for (X509TrustManager tm : tms) {
 
 			try {
 
+				log.debug("Checking server certificate chain " + chain + " against " + tm);
 				tm.checkClientTrusted(chain, authType);
 				return;
-			} catch (CertificateException ex2) {
+			} catch (CertificateException ex) {
 
-				ex = ex2;
+				cex = ex;
+			} catch (RuntimeException ex) {
+
+				rex = ex;
 			}
 		}
 
-		if (ex != null) throw ex;
+		if (cex != null) throw cex;
+		if (rex != null) throw rex;
 	}  
 
 
 	@Override
 	public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {  
 
-		CertificateException ex = null;
+		CertificateException cex = null;
+		RuntimeException rex = null;
 
 		for (X509TrustManager tm : tms) {
 
 			try {
 
+				log.debug("Checking server certificate chain " + chain + " against " + tm);
 				tm.checkServerTrusted(chain, authType);
 				return;
-			} catch (CertificateException ex2) {
+			} catch (CertificateException ex) {
 
-				ex = ex2;
+				cex = ex;
+			} catch (RuntimeException ex) {
+
+				rex = ex;
 			}
 		}
 
-		if (ex != null) throw ex;
+		if (cex != null) throw cex;
+		if (rex != null) throw rex;
 	}  
 
 	@Override
@@ -81,8 +100,8 @@ public class XDI2X509TrustManager implements X509TrustManager {
 
 		// create XDI2 trust manager
 
-		KeyStore ks2 = KeyStore.getInstance("JKS");
-		ks2.load(XDI2X509TrustManager.class.getResourceAsStream("cacerts"), "changeit".toCharArray());
+		KeyStore ks2 = KeyStore.getInstance("JKS", "SUN");
+		ks2.load(XDI2X509TrustManager.class.getResourceAsStream("cacerts.jks"), "changeit".toCharArray());
 
 		TrustManagerFactory tmf2 = TrustManagerFactory.getInstance("SunX509", "SunJSSE");
 		tmf2.init(ks2);
