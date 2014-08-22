8851be9d9bfec090525298c13bd87641e60591ce
peacekeeper <markus.sabadello@gmail.com>
2014-08-22 12:01:05 +0400

    comment fix

--- core/src/main/java/xdi2/core/features/linkcontracts/evaluation/PolicyEvaluationContext.java
+++ core/src/main/java/xdi2/core/features/linkcontracts/evaluation/PolicyEvaluationContext.java
@@ -7,7 +7,7 @@ import xdi2.core.xri3.XDI3Statement;
 
 /**
  * This interface provides a context for evaluating an XDI policy.
- * It is able to look up a context node XRI, context node, and relation.
+ * It is able to look up a context node address, context node, and statement.
  * This is used to determine if policies, policy statements, and conditions evaluate to true or false.
  */
 public interface PolicyEvaluationContext {
