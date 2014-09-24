29dae4ca65f6bedb2d2fde2f103a40544c32081b
peacekeeper <markus@projectdanube.org>
2014-09-24 08:47:50 +0200

    WIP reintroducing $add and $mod

--- messaging/src/main/java/xdi2/messaging/target/impl/AbstractContextHandler.java
+++ messaging/src/main/java/xdi2/messaging/target/impl/AbstractContextHandler.java
@@ -1,5 +1,10 @@
 package xdi2.messaging.target.impl;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import xdi2.core.syntax.XDIAddress;
 import xdi2.core.syntax.XDIStatement;
 import xdi2.core.util.XDIAddressUtil;
@@ -7,6 +12,8 @@ import xdi2.messaging.AddOperation;
 import xdi2.messaging.DelOperation;
 import xdi2.messaging.DoOperation;
 import xdi2.messaging.GetOperation;
+import xdi2.messaging.Message;
+import xdi2.messaging.MessageEnvelope;
 import xdi2.messaging.MessageResult;
 import xdi2.messaging.ModOperation;
 import xdi2.messaging.Operation;
@@ -15,8 +22,13 @@ import xdi2.messaging.context.ExecutionContext;
 import xdi2.messaging.exceptions.Xdi2MessagingException;
 import xdi2.messaging.target.ContextHandler;
 import xdi2.messaging.target.impl.graph.GraphContextHandler;
+import xdi2.messaging.target.interceptor.impl.MessagePolicyInterceptor;
+import xdi2.messaging.target.interceptor.impl.ToInterceptor;
+import xdi2.messaging.target.interceptor.impl.linkcontract.LinkContractInterceptor;
 
 public abstract class AbstractContextHandler implements ContextHandler {
+
+	private static final Logger log = LoggerFactory.getLogger(AbstractContextHandler.class);
 
 	public AbstractContextHandler() {
 
@@ -57,10 +69,40 @@ public abstract class AbstractContextHandler implements ContextHandler {
 
 	public void executeAddOnAddress(XDIAddress targetAddress, AddOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// $get feedback on target address
+
+		MessageResult feedbackMessageResult = feedbackGetTargetAddressOrStatement(targetAddress, null, operation, executionContext);
+
+		if (feedbackMessageResult.isEmpty()) {
+
+			// execute a $set operation
+
+			SetOperation setOperation = /* TODO */ null;
+
+			this.executeSetOnAddress(targetAddress, setOperation, feedbackMessageResult, executionContext);
+		} else {
+
+			throw new Xdi2MessagingException("Target statement exists already: " + operation, null, executionContext);
+		}
 	}
 
 	public void executeModOnAddress(XDIAddress targetAddress, ModOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// $get feedback on target address
+
+		MessageResult feedbackMessageResult = feedbackGetTargetAddressOrStatement(targetAddress, null, operation, executionContext);
+
+		if (feedbackMessageResult.isEmpty()) {
+
+			throw new Xdi2MessagingException("Target address does not exist: " + operation, null, executionContext);
+		} else {
+
+			// execute a $set operation
+
+			SetOperation setOperation = /* TODO */ null;
+
+			this.executeSetOnAddress(targetAddress, setOperation, feedbackMessageResult, executionContext);
+		}
 	}
 
 	public void executeDelOnAddress(XDIAddress targetAddress, DelOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
@@ -174,6 +216,8 @@ public abstract class AbstractContextHandler implements ContextHandler {
 
 	public void executeGetOnContextNodeStatement(XDIStatement targetStatement, GetOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// run a $get on the address, then filter for the statement
+
 		XDIAddress targetAddress = targetStatement.getContextNodeXDIAddress();
 
 		MessageResult tempMessageResult = new MessageResult();
@@ -185,6 +229,8 @@ public abstract class AbstractContextHandler implements ContextHandler {
 
 	public void executeSetOnContextNodeStatement(XDIStatement targetStatement, SetOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// a $set on a context node statement is the same as a $set on the address
+
 		XDIAddress targetAddress = XDIAddressUtil.concatXDIAddresses(targetStatement.getContextNodeXDIAddress(), targetStatement.getContextNodeXDIArc());
 
 		this.executeSetOnAddress(targetAddress, operation, messageResult, executionContext);
@@ -192,13 +238,45 @@ public abstract class AbstractContextHandler implements ContextHandler {
 
 	public void executeAddOnContextNodeStatement(XDIStatement contextNodeStatement, AddOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// $get feedback on target statement
+
+		MessageResult feedbackMessageResult = feedbackGetTargetAddressOrStatement(null, contextNodeStatement, operation, executionContext);
+
+		if (feedbackMessageResult.isEmpty()) {
+
+			// execute a $set operation
+
+			SetOperation setOperation = /* TODO */ null;
+
+			this.executeSetOnContextNodeStatement(contextNodeStatement, setOperation, feedbackMessageResult, executionContext);
+		} else {
+
+			throw new Xdi2MessagingException("Target statement exists already: " + operation, null, executionContext);
+		}
 	}
 
 	public void executeModOnContextNodeStatement(XDIStatement contextNodeStatement, ModOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// $get feedback on target statement
+
+		MessageResult feedbackMessageResult = feedbackGetTargetAddressOrStatement(null, contextNodeStatement, operation, executionContext);
+
+		if (feedbackMessageResult.isEmpty()) {
+
+			throw new Xdi2MessagingException("Target statement does not exist: " + operation, null, executionContext);
+		} else {
+
+			// execute a $set operation
+
+			SetOperation setOperation = /* TODO */ null;
+
+			this.executeSetOnContextNodeStatement(contextNodeStatement, setOperation, feedbackMessageResult, executionContext);
+		}
 	}
 
 	public void executeDelOnContextNodeStatement(XDIStatement targetStatement, DelOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
+
+		// a $del on a context node statement is the same as a $del on the address
 
 		XDIAddress targetAddress = XDIAddressUtil.concatXDIAddresses(targetStatement.getContextNodeXDIAddress(), targetStatement.getContextNodeXDIArc());
 
@@ -215,6 +293,8 @@ public abstract class AbstractContextHandler implements ContextHandler {
 
 	public void executeGetOnRelationStatement(XDIStatement targetStatement, GetOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// run a $get on the address, then filter for the statement
+
 		XDIAddress targetAddress = targetStatement.getContextNodeXDIAddress();
 
 		MessageResult tempMessageResult = new MessageResult();
@@ -230,10 +310,40 @@ public abstract class AbstractContextHandler implements ContextHandler {
 
 	public void executeAddOnRelationStatement(XDIStatement relationStatement, AddOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// $get feedback on target statement
+
+		MessageResult feedbackMessageResult = feedbackGetTargetAddressOrStatement(null, relationStatement, operation, executionContext);
+
+		if (feedbackMessageResult.isEmpty()) {
+
+			// execute a $set operation
+
+			SetOperation setOperation = /* TODO */ null;
+
+			this.executeSetOnRelationStatement(relationStatement, setOperation, feedbackMessageResult, executionContext);
+		} else {
+
+			throw new Xdi2MessagingException("Target statement exists already: " + operation, null, executionContext);
+		}
 	}
 
 	public void executeModOnRelationStatement(XDIStatement relationStatement, ModOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// $get feedback on target statement
+
+		MessageResult feedbackMessageResult = feedbackGetTargetAddressOrStatement(null, relationStatement, operation, executionContext);
+
+		if (feedbackMessageResult.isEmpty()) {
+
+			throw new Xdi2MessagingException("Target statement does not exist: " + operation, null, executionContext);
+		} else {
+
+			// execute a $set operation
+
+			SetOperation setOperation = /* TODO */ null;
+
+			this.executeSetOnRelationStatement(relationStatement, setOperation, feedbackMessageResult, executionContext);
+		}
 	}
 
 	public void executeDelOnRelationStatement(XDIStatement targetStatement, DelOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
@@ -250,6 +360,8 @@ public abstract class AbstractContextHandler implements ContextHandler {
 
 	public void executeGetOnLiteralStatement(XDIStatement targetStatement, GetOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// run a $get on the address, then filter for the statement
+
 		XDIAddress targetAddress = targetStatement.getContextNodeXDIAddress();
 
 		MessageResult tempMessageResult = new MessageResult();
@@ -265,10 +377,40 @@ public abstract class AbstractContextHandler implements ContextHandler {
 
 	public void executeAddOnLiteralStatement(XDIStatement literalStatement, AddOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// $get feedback on target statement
+
+		MessageResult feedbackMessageResult = feedbackGetTargetAddressOrStatement(null, literalStatement, operation, executionContext);
+
+		if (feedbackMessageResult.isEmpty()) {
+
+			// execute a $set operation
+
+			SetOperation setOperation = /* TODO */ null;
+
+			this.executeSetOnLiteralStatement(literalStatement, setOperation, feedbackMessageResult, executionContext);
+		} else {
+
+			throw new Xdi2MessagingException("Target statement exists already: " + operation, null, executionContext);
+		}
 	}
 
 	public void executeModOnLiteralStatement(XDIStatement literalStatement, ModOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+		// $get feedback on target statement
+
+		MessageResult feedbackMessageResult = feedbackGetTargetAddressOrStatement(null, literalStatement, operation, executionContext);
+
+		if (feedbackMessageResult.isEmpty()) {
+
+			throw new Xdi2MessagingException("Target statement does not exist: " + operation, null, executionContext);
+		} else {
+
+			// execute a $set operation
+
+			SetOperation setOperation = /* TODO */ null;
+
+			this.executeSetOnLiteralStatement(literalStatement, setOperation, feedbackMessageResult, executionContext);
+		}
 	}
 
 	public void executeDelOnLiteralStatement(XDIStatement targetStatement, DelOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
@@ -277,5 +419,74 @@ public abstract class AbstractContextHandler implements ContextHandler {
 
 	public void executeDoOnLiteralStatement(XDIStatement targetStatement, DoOperation operation, MessageResult messageResult, ExecutionContext executionContext) throws Xdi2MessagingException {
 
+	}
+
+	/*
+	 * Feedback methods
+	 */
+
+	private static MessageResult feedbackGetTargetAddressOrStatement(XDIAddress targetAddress, XDIStatement targetStatement, Operation operation, ExecutionContext executionContext) throws Xdi2MessagingException {
+
+		if (targetAddress == null && targetStatement == null) throw new IllegalArgumentException();
+		if (targetAddress != null && targetStatement != null) throw new IllegalArgumentException();
+
+		if (log.isDebugEnabled()) log.debug("Initiating $get feedback to get target address/statement: " + targetAddress + ", " + targetStatement);
+
+		// prepare messaging target
+
+		AbstractMessagingTarget messagingTarget = (AbstractMessagingTarget) executionContext.getCurrentMessagingTarget();
+
+		// prepare feedback message result
+
+		MessageResult feedbackMessageResult = new MessageResult();
+
+		// prepare feedback message
+
+		Message feedbackMessage = new MessageEnvelope().createMessage(operation.getSenderXDIAddress());
+
+		Operation feedbackOperation = null;
+
+		if (targetAddress != null) feedbackOperation = feedbackMessage.createGetOperation(targetAddress);
+		if (targetStatement != null) feedbackOperation = feedbackMessage.createGetOperation(targetStatement);
+
+		feedbackOperation.setParameter(GetOperation.XDI_ADD_PARAMETER_DEREF, Boolean.TRUE);
+
+		// feedback
+
+		Map<String, Object> messageAttributes = null;
+		Map<String, Object> operationAttributes = null;
+
+		try {
+
+			// before feedback: tweak the execution context and messaging target
+
+			ToInterceptor toInterceptor = messagingTarget.getInterceptors().getInterceptor(ToInterceptor.class);
+			if (toInterceptor != null) toInterceptor.setDisabledForMessage(feedbackMessage);
+
+			LinkContractInterceptor linkContractInterceptor = messagingTarget.getInterceptors().getInterceptor(LinkContractInterceptor.class);
+			if (linkContractInterceptor != null) linkContractInterceptor.setDisabledForMessage(feedbackMessage);
+
+			MessagePolicyInterceptor messagePolicyInterceptor = messagingTarget.getInterceptors().getInterceptor(MessagePolicyInterceptor.class);
+			if (messagePolicyInterceptor != null) messagePolicyInterceptor.setDisabledForMessage(feedbackMessage);
+
+			messageAttributes = executionContext.getMessageAttributes();
+			operationAttributes = executionContext.getOperationAttributes();
+
+			// execute feedback messages
+
+			messagingTarget.execute(feedbackMessage, feedbackMessageResult, executionContext);
+		} finally {
+
+			// after feedback: restore the execution context and messaging target
+
+			if (messageAttributes != null) executionContext.setMessageAttributes(messageAttributes);
+			if (operationAttributes != null) executionContext.setOperationAttributes(operationAttributes);
+		}
+
+		// done
+
+		if (log.isDebugEnabled()) log.debug("Completed $get feedback to get target address/statement: " + targetAddress + ", " + targetStatement + ", message result: " + feedbackMessageResult);
+
+		return feedbackMessageResult;
 	}
 }
