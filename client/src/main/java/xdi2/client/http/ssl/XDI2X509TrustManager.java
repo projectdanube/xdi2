cfb82d8674408502ef3f29aa57dda1c0ab132745
peacekeeper <markus@projectdanube.org>
2015-06-20 22:56:43 +0200

    remove SUN specific references

--- client/src/main/java/xdi2/client/http/ssl/XDI2X509TrustManager.java
+++ client/src/main/java/xdi2/client/http/ssl/XDI2X509TrustManager.java
@@ -91,7 +91,7 @@ public class XDI2X509TrustManager implements X509TrustManager {
 
 			// get default trust manager
 
-			TrustManagerFactory tmf1 = TrustManagerFactory.getInstance("SunX509", "SunJSSE");
+			TrustManagerFactory tmf1 = TrustManagerFactory.getInstance("X509");
 			tmf1.init((KeyStore) null);
 
 			TrustManager tms1[] = tmf1.getTrustManagers();
@@ -99,10 +99,10 @@ public class XDI2X509TrustManager implements X509TrustManager {
 
 			// create XDI2 trust manager
 
-			KeyStore ks2 = KeyStore.getInstance("JKS", "SUN");
+			KeyStore ks2 = KeyStore.getInstance("JKS");
 			ks2.load(XDI2X509TrustManager.class.getResourceAsStream("cacerts.jks"), "changeit".toCharArray());
 
-			TrustManagerFactory tmf2 = TrustManagerFactory.getInstance("SunX509", "SunJSSE");
+			TrustManagerFactory tmf2 = TrustManagerFactory.getInstance("X509");
 			tmf2.init(ks2);
 
 			TrustManager tms2[] = tmf2.getTrustManagers();
