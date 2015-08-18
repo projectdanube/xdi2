23d9b0426af05cf8101f3af7701aa97cc6539a74
peacekeeper <markus@projectdanube.org>
2015-08-18 11:34:39 +0200

    more work on normalizing graphs

--- core/src/main/java/xdi2/core/io/Normalization.java
+++ core/src/main/java/xdi2/core/io/Normalization.java
@@ -1,6 +1,7 @@
 package xdi2.core.io;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Properties;
 
@@ -9,8 +10,10 @@ import org.slf4j.LoggerFactory;
 
 import xdi2.core.ContextNode;
 import xdi2.core.Graph;
+import xdi2.core.exceptions.Xdi2ParseException;
 import xdi2.core.exceptions.Xdi2RuntimeException;
 import xdi2.core.impl.memory.MemoryGraphFactory;
+import xdi2.core.io.readers.XDIJSONReader;
 import xdi2.core.io.writers.XDIJSONWriter;
 import xdi2.core.util.CopyUtil;
 import xdi2.core.util.CopyUtil.CopyStrategy;
@@ -19,7 +22,8 @@ public class Normalization {
 
 	private static Logger log = LoggerFactory.getLogger(Normalization.class.getName());
 
-	private static final XDIWriter XDIWRITER;
+	public static final XDIWriter XDIWRITER;
+	public static final XDIReader XDIREADER;
 
 	static {
 
@@ -29,13 +33,15 @@ public class Normalization {
 		parameters.setProperty(XDIWriterRegistry.PARAMETER_PRETTY, "0");
 
 		XDIWRITER = new XDIJSONWriter(parameters);
+
+		XDIREADER = new XDIJSONReader(null);
 	}
 
 	/**
 	 * Returns the normalized serialization string of a context node, to be used
 	 * e.g. for signatures and encryptions.
 	 */
-	public static String normalize(ContextNode contextNode, CopyStrategy copyStrategy) {
+	public static String serialize(ContextNode contextNode, CopyStrategy copyStrategy) {
 
 		Graph tempGraph;
 
@@ -68,26 +74,49 @@ public class Normalization {
 	 * Returns the normalized serialization string of a context node, to be used
 	 * e.g. for signatures and encryptions.
 	 */
-	public static String normalize(ContextNode contextNode) {
+	public static String serialize(ContextNode contextNode) {
 
-		return normalize(contextNode, null);
+		return serialize(contextNode, null);
 	}
 
 	/**
 	 * Returns the normalized serialization string of a graph, to be used
 	 * e.g. for signatures and encryptions.
 	 */
-	public static String normalize(Graph graph, CopyStrategy copyStrategy) {
+	public static String serialize(Graph graph, CopyStrategy copyStrategy) {
 
-		return normalize(graph.getRootContextNode(), copyStrategy);
+		return serialize(graph.getRootContextNode(), copyStrategy);
 	}
 
 	/**
 	 * Returns the normalized serialization string of a graph, to be used
 	 * e.g. for signatures and encryptions.
 	 */
-	public static String normalize(Graph graph) {
+	public static String serialize(Graph graph) {
 
-		return normalize(graph, null);
+		return serialize(graph, null);
+	}
+
+	/**
+	 * Returns the graph from a normalized string.
+	 */
+	public static Graph deserialize(String string) throws Xdi2ParseException, IOException {
+
+		Graph tempGraph = MemoryGraphFactory.getInstance().openGraph();
+
+		try {
+
+			XDIREADER.read(tempGraph, new StringReader(string));
+		} catch (Xdi2ParseException ex) {
+
+			tempGraph.close();
+			throw ex;
+		} catch (IOException ex) {
+
+			tempGraph.close();
+			throw ex;
+		}
+
+		return tempGraph;
 	}
 }
