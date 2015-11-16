2626e40050cb7762cf86c330ba3d64e74b5285f4
peacekeeper <markus@projectdanube.org>
2015-11-16 13:44:30 +0100

    WIP variables

--- core/src/main/java/xdi2/core/features/variables/Variables.java
+++ core/src/main/java/xdi2/core/features/variables/Variables.java
@@ -1,9 +1,8 @@
-package xdi2.core.util;
+package xdi2.core.features.variables;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import xdi2.core.constants.XDIConstants;
 import xdi2.core.features.nodetypes.XdiAttributeCollection;
 import xdi2.core.features.nodetypes.XdiAttributeInstanceOrdered;
 import xdi2.core.features.nodetypes.XdiAttributeInstanceUnordered;
@@ -18,19 +17,17 @@ import xdi2.core.features.nodetypes.XdiInnerRoot;
 import xdi2.core.features.nodetypes.XdiPeerRoot;
 import xdi2.core.features.nodetypes.XdiVariable;
 import xdi2.core.syntax.XDIAddress;
-import xdi2.core.syntax.XDIArc;
 
 /**
  * Utility methods for working with variables.
  * 
  * @author markus
  */
-// TODO: FIX THESE for new variable syntax
-public final class VariableUtil {
+public final class Variables {
 
-	private static final Logger log = LoggerFactory.getLogger(VariableUtil.class);
+	private static final Logger log = LoggerFactory.getLogger(Variables.class);
 
-	private VariableUtil() { }
+	private Variables() { }
 
 	/**
 	 * Checks if a variable can match multiple contexts.
@@ -53,13 +50,11 @@ public final class VariableUtil {
 	 * @param arc The arc to match the variable against.
 	 * @return True, if the variable matches the arc.
 	 */
-	@Deprecated
-	// TODO: this is just a start
 	public static boolean matches(XdiVariable<?> xdiVariable, XdiContext<?> xdiContext) {
 
 		if (log.isTraceEnabled()) log.trace("Matching variable " + xdiVariable + " (" + xdiVariable.getClass().getCanonicalName() + ") against context " + xdiContext + " (" + xdiContext.getClass().getCanonicalName() + ")");
 
-		if (XdiCommonVariable.class.isAssignableFrom(xdiVariable.getClass())) return true;
+		if (XdiCommonVariable.class.equals(xdiVariable.getClass())) return true;
 
 		if (XdiPeerRoot.Variable.class.equals(xdiVariable.getClass()) && XdiPeerRoot.class.isAssignableFrom(xdiContext.getClass())) return true;
 		if (XdiInnerRoot.Variable.class.equals(xdiVariable.getClass()) && XdiInnerRoot.class.isAssignableFrom(xdiContext.getClass())) return true;
@@ -67,19 +62,14 @@ public final class VariableUtil {
 		if (XdiEntityCollection.Variable.class.equals(xdiVariable.getClass()) && XdiEntityCollection.class.isAssignableFrom(xdiContext.getClass())) return true;
 		if (XdiAttributeCollection.Variable.class.equals(xdiVariable.getClass()) && XdiAttributeCollection.class.isAssignableFrom(xdiContext.getClass())) return true;
 
-		if (XdiEntityInstanceOrdered.Variable.class.isAssignableFrom(xdiVariable.getClass()) && XdiEntityInstanceOrdered.class.isAssignableFrom(xdiContext.getClass())) return true;
-		if (XdiEntityInstanceUnordered.Variable.class.isAssignableFrom(xdiVariable.getClass()) && XdiEntityInstanceUnordered.class.isAssignableFrom(xdiContext.getClass())) return true;
-		if (XdiAttributeInstanceOrdered.Variable.class.isAssignableFrom(xdiVariable.getClass()) && XdiAttributeInstanceOrdered.class.isAssignableFrom(xdiContext.getClass())) return true;
-		if (XdiAttributeInstanceUnordered.Variable.class.isAssignableFrom(xdiVariable.getClass()) && XdiAttributeInstanceUnordered.class.isAssignableFrom(xdiContext.getClass())) return true;
+		if (XdiEntityInstanceOrdered.Variable.class.equals(xdiVariable.getClass()) && XdiEntityInstanceOrdered.class.isAssignableFrom(xdiContext.getClass())) return true;
+		if (XdiEntityInstanceUnordered.Variable.class.equals(xdiVariable.getClass()) && XdiEntityInstanceUnordered.class.isAssignableFrom(xdiContext.getClass())) return true;
+		if (XdiAttributeInstanceOrdered.Variable.class.equals(xdiVariable.getClass()) && XdiAttributeInstanceOrdered.class.isAssignableFrom(xdiContext.getClass())) return true;
+		if (XdiAttributeInstanceUnordered.Variable.class.equals(xdiVariable.getClass()) && XdiAttributeInstanceUnordered.class.isAssignableFrom(xdiContext.getClass())) return true;
 
-		if (XdiEntitySingleton.Variable.class.isAssignableFrom(xdiVariable.getClass()) && XdiEntitySingleton.class.isAssignableFrom(xdiContext.getClass())) return true;
-		if (XdiAttributeSingleton.Variable.class.isAssignableFrom(xdiVariable.getClass()) && XdiAttributeSingleton.class.isAssignableFrom(xdiContext.getClass())) return true;
+		if (XdiEntitySingleton.Variable.class.equals(xdiVariable.getClass()) && XdiEntitySingleton.class.isAssignableFrom(xdiContext.getClass())) return true;
+		if (XdiAttributeSingleton.Variable.class.equals(xdiVariable.getClass()) && XdiAttributeSingleton.class.isAssignableFrom(xdiContext.getClass())) return true;
 
 		return false;
-	}
-
-	public static boolean matches(XDIAddress XDIvariableAddress, XDIArc XDIaddress) {
-
-		return XDIConstants.XDI_ADD_COMMON_VARIABLE.equals(XDIvariableAddress);
 	}
 }
