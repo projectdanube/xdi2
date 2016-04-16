38c934be621b0fb42cb57f862c90a96fcedb5d17
peacekeeper <markus@projectdanube.org>
2016-04-16 17:23:49 +0200

    comment

--- transport-uri/src/main/java/xdi2/transport/impl/uri/UriTransportResponse.java
+++ transport-uri/src/main/java/xdi2/transport/impl/uri/UriTransportResponse.java
@@ -4,7 +4,7 @@ import xdi2.transport.TransportResponse;
 import xdi2.transport.impl.AbstractTransportResponse;
 
 /**
- * This class represents an XDI request to a URI (e.g. HTTP or WebSocket).
+ * This class represents an XDI response from a URI (e.g. HTTP or WebSocket).
  * 
  * @author markus
  */
